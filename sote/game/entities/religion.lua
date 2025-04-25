6cd403f4f41e4e5f0b18c189a940be5918e96fcf
Jose <117308298+Hispano1919@users.noreply.github.com>
2025-04-25 18:21:04 +0200

    Update religion.lua

--- sote/game/entities/religion.lua
+++ sote/game/entities/religion.lua
@@ -1,59 +1,88 @@
+local language_utils = require "game.entities.language".Language
+
 local cl = {}
 
----@alias BurialRites 'cremation' | 'burial' | 'none'
 
----@class (exact) Religion
----@field __index Religion
----@field name string
----@field r number
----@field g number
----@field b number
+-- Módulo para manejar espíritus (Lua puro)
+local spirit_module = require "game.entities.spirit"
 
----@class (exact) Faith
----@field __index Faith
----@field name string
----@field r number
----@field g number
----@field b number
----@field religion Religion
----@field burial_rites BurialRites
+cl.Spirit = {}
+cl.Spirit.__index = cl.Spirit
+---@param domain string
+---@param culture culture_id
+---@return spirit_id
+function cl.Spirit:new(domain,culture)
+    local spirit = spirit_module.create_spirit()
+    
+    spirit_module.set_name(spirit, language_utils.get_random_name(DATA.culture_get_language(culture)))
+    spirit_module.set_domain(spirit, domain)
+    spirit_module.set_rank(spirit, 1)
+    
+    return spirit
+end
 
----@class Religion
+
 cl.Religion = {}
 cl.Religion.__index = cl.Religion
----@param culture Culture
----@return Religion
+---@param culture culture_id
+---@return religion_id
 function cl.Religion:new(culture)
-	local o = {}
+	local religion = DATA.create_religion()
 
-	o.r = love.math.random()
-	o.g = love.math.random()
-	o.b = love.math.random()
-	o.name = culture.language:get_random_faith_name()
+	DATA.religion_set_r(religion, love.math.random())
+	DATA.religion_set_g(religion, love.math.random())
+	DATA.religion_set_b(religion, love.math.random())
 
-	setmetatable(o, cl.Religion)
-	return o
+	DATA.religion_set_name(religion, language_utils.get_random_faith_name(DATA.culture_get_language(culture)))
+
+	return religion
 end
 
 ---@class Faith
 cl.Faith = {}
 cl.Faith.__index = cl.Faith
----@param religion Religion
----@param culture Culture
----@return Faith
+
+---@param religion religion_id
+---@param culture culture_id
+---@return faith_id
 function cl.Faith:new(religion, culture)
-	---@type Faith
-	local o = {}
+	local faith = DATA.create_faith()
 
-	o.r = religion.r
-	o.g = religion.g
-	o.b = religion.b
-	o.religion = religion
-	o.name = culture.language:get_random_faith_name()
-	o.burial_rites = 'burial'
+	DATA.faith_set_religion(faith, religion)  -- <- Línea añadida
 
-	setmetatable(o, cl.Faith)
-	return o
+
+	DATA.faith_set_r(faith, DATA.religion_get_r(religion))
+	DATA.faith_set_g(faith, DATA.religion_get_g(religion))
+	DATA.faith_set_b(faith, DATA.religion_get_b(religion))
+
+	DATA.force_create_subreligion(religion, faith)
+	DATA.faith_set_name(faith, language_utils.get_random_faith_name(DATA.culture_get_language(culture)))
+	DATA.faith_set_burial_rites(faith, BURIAL_RIGHTS.BURIAL)
+
+	DATA.faith_set_birth_rites(faith, BIRTH_RITES.BLESSING)
+    DATA.faith_set_passage_rites(faith, PASSAGE_RITES.COMING_OF_AGE)
+    DATA.faith_set_disease_rites(faith, DISEASE_RITES.HEALING_PRAYER)
+
+	local spirit = cl.Spirit:new("Fuego",culture)  -- <-- Agregar local aquí
+	DATA.faith_set_spirit(faith, spirit)  -- Necesitarías crear esta función en tu módulo DATA
+
+
+	return faith
+end
+
+---@class Rite
+cl.Rite = {}
+cl.Rite.__index = cl.Rite
+---@param faith faith_id
+---@param culture culture_id
+---@return rite_id
+function cl.Rite:new(faith, culture)
+    local rite = DATA.create_rite()  -- Necesitarías implementar esto
+    
+    DATA.rite_set_faith(rite, faith)
+    DATA.rite_set_name(rite, language_utils.get_random_faith_name(DATA.culture_get_language(culture)))
+    
+    return rite
 end
 
 return cl
