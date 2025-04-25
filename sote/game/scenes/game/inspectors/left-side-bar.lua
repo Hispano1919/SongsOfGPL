bfea9fb5917edcaa77a30ea88d108b2179e07536
Jose <117308298+Hispano1919@users.noreply.github.com>
2025-04-25 18:20:12 +0200

    Update left-side-bar.lua

--- sote/game/scenes/game/inspectors/left-side-bar.lua
+++ sote/game/scenes/game/inspectors/left-side-bar.lua
@@ -1,148 +1,88 @@
-local ui = require "engine.ui"
-local ut = require "game.ui-utils"
+local language_utils = require "game.entities.language".Language
 
-local office_triggers = require "game.raws.triggers.offices"
-
-local inspector = {}
-
-local function get_main_panel()
-    local fs = ui.fullscreen()
-	local panel = fs:subrect(0, ut.BASE_HEIGHT * 2, ut.BASE_HEIGHT * 2, fs.height, "left", "up")
-	return panel
-end
-
-function inspector.mask()
-    if ui.trigger(get_main_panel()) then
-		return false
-	else
-		return true
-	end
-end
+local cl = {}
 
 
----comment
----@param gam GameScene
-function inspector.draw(gam)
-    local rect = get_main_panel()
-    local base_unit = ut.BASE_HEIGHT * 2
-    ui.panel(rect)
+-- Módulo para manejar espíritus (Lua puro)
+local spirit_module = require "game.entities.spirit"
 
-    local layout = ui.layout_builder()
-        :vertical()
-        :position(rect.x, rect.y)
-        :spacing(0)
-        :build()
-
-    local inspectors = {
-        "macrobuilder",
-        "macrodecision",
-        "army",
-        "character-decisions",
-        "warband",
-        "property",
-        "market",
-        "preferences",
-        "quests"
-    }
-
-    local inspector_icons = {
-        ["macrobuilder"] = ASSETS.icons["hammer-drop.png"],
-        ["macrodecision"] = ASSETS.icons["horizon-road.png"],
-        ["army"] = ASSETS.icons["guards.png"],
-        ["character-decisions"] = ASSETS.icons["envelope.png"],
-        ["warband"] = ASSETS.icons["barbute.png"],
-        ["property"] = ASSETS.icons["bank.png"],
-        ["market"] = ASSETS.icons["scales.png"],
-        ["preferences"] = ASSETS.icons["shrug.png"],
-        ["quests"] =ASSETS.icons["coins.png"]
-    }
-
-    local inspector_tooltips = {
-        ["macrobuilder"] = "Plan development of your estates",
-        ["macrodecision"] = "Target province",
-        ["army"] = "View local warriors",
-        ["character-decisions"] = "Actions",
-        ["warband"] = "View your warband",
-        ["property"] = "Assess your property",
-        ["market"] = "View local market",
-        ["preferences"] = "Decide your preferences",
-        ["quests"] = "View available quests"
-    }
-
-    local inspector_visible = {
-        ["macrobuilder"] = function ()
-            if WORLD.player_character == nil then return false end
-            return true
-        end,
-        ["macrodecision"] = function ()
-            if WORLD.player_character == nil then return false end
-            return true
-        end,
-        ["army"] = function ()
-            if WORLD.player_character == nil then return false end
-            return true
-        end,
-        ["character-decisions"] = function ()
-            if WORLD.player_character == nil then return false end
-            return true
-        end,
-        ["warband"] = function ()
-            if WORLD.player_character == nil then return false end
-            if WORLD.player_character.leading_warband then return true end
-            if office_triggers.guard_leader(WORLD.player_character, WORLD.player_character.realm) then return true end
-            return false
-        end,
-        ["property"] = function ()
-            if WORLD.player_character == nil then return false end
-            return true
-        end,
-        ["market"] = function ()
-            if WORLD.player_character == nil then return false end
-            return true
-        end,
-        ["preferences"] = function ()
-            return true
-        end,
-        ["quests"] = function ()
-            if WORLD.player_character == nil then return false end
-            return true
-        end
-    }
-
-    for _, inspector in pairs(inspectors) do
-        local active = gam.inspector == inspector
-        local icon = inspector_icons[inspector]
-        local tooltip = inspector_tooltips[inspector]
-
-        local check = inspector_visible[inspector]
-
-        if check() then
-            if ut.icon_button(icon, layout:next(base_unit, base_unit), tooltip, true, active) then
-                if active then
-                    gam.inspector = nil
-                else
-                    gam.inspector = inspector
-
-                    local character = WORLD.player_character
-                    if character and gam.inspector == "market" then
-                        gam.selected.province = character.province
-                    end
-                end
-            end
-        end
-    end
-
-
-    local fs = ui.fullscreen()
-    local rect_for_menu = fs:subrect(0, 0, base_unit, base_unit, "left", "down")
-	if ut.icon_button(
-			ASSETS.icons["gear.png"],
-			rect_for_menu,
-			"Open menu."
-	) then
-		gam.inspector = "confirm-exit"
-	end
+cl.Spirit = {}
+cl.Spirit.__index = cl.Spirit
+---@param domain string
+---@param culture culture_id
+---@return spirit_id
+function cl.Spirit:new(domain,culture)
+    local spirit = spirit_module.create_spirit()
+    
+    spirit_module.set_name(spirit, language_utils.get_random_name(DATA.culture_get_language(culture)))
+    spirit_module.set_domain(spirit, domain)
+    spirit_module.set_rank(spirit, 1)
+    
+    return spirit
 end
 
 
-return inspector
+cl.Religion = {}
+cl.Religion.__index = cl.Religion
+---@param culture culture_id
+---@return religion_id
+function cl.Religion:new(culture)
+	local religion = DATA.create_religion()
+
+	DATA.religion_set_r(religion, love.math.random())
+	DATA.religion_set_g(religion, love.math.random())
+	DATA.religion_set_b(religion, love.math.random())
+
+	DATA.religion_set_name(religion, language_utils.get_random_faith_name(DATA.culture_get_language(culture)))
+
+	return religion
+end
+
+---@class Faith
+cl.Faith = {}
+cl.Faith.__index = cl.Faith
+
+---@param religion religion_id
+---@param culture culture_id
+---@return faith_id
+function cl.Faith:new(religion, culture)
+	local faith = DATA.create_faith()
+
+	DATA.faith_set_religion(faith, religion)  -- <- Línea añadida
+
+
+	DATA.faith_set_r(faith, DATA.religion_get_r(religion))
+	DATA.faith_set_g(faith, DATA.religion_get_g(religion))
+	DATA.faith_set_b(faith, DATA.religion_get_b(religion))
+
+	DATA.force_create_subreligion(religion, faith)
+	DATA.faith_set_name(faith, language_utils.get_random_faith_name(DATA.culture_get_language(culture)))
+	DATA.faith_set_burial_rites(faith, BURIAL_RIGHTS.BURIAL)
+
+	DATA.faith_set_birth_rites(faith, BIRTH_RITES.BLESSING)
+    DATA.faith_set_passage_rites(faith, PASSAGE_RITES.COMING_OF_AGE)
+    DATA.faith_set_disease_rites(faith, DISEASE_RITES.HEALING_PRAYER)
+
+	local spirit = cl.Spirit:new("Fuego",culture)  -- <-- Agregar local aquí
+	DATA.faith_set_spirit(faith, spirit)  -- Necesitarías crear esta función en tu módulo DATA
+
+
+	return faith
+end
+
+---@class Rite
+cl.Rite = {}
+cl.Rite.__index = cl.Rite
+---@param faith faith_id
+---@param culture culture_id
+---@return rite_id
+function cl.Rite:new(faith, culture)
+    local rite = DATA.create_rite()  -- Necesitarías implementar esto
+    
+    DATA.rite_set_faith(rite, faith)
+    DATA.rite_set_name(rite, language_utils.get_random_faith_name(DATA.culture_get_language(culture)))
+    
+    return rite
+end
+
+return cl
